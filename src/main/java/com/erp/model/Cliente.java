fa4710ff2af7ccdd2e00165ad5e74f66dbcf5278
book5 <noeconvil@alu.edu.gva.es>
2025-07-21 13:53:23 +0200

    Empezamos con cliente para luego implementar dentro descuentos

---
+++ src/main/java/com/erp/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.erp.model;
+
+public class Cliente {
+    
+    private Integer id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String cifnif;
+    private String tipoCliente;
+
+
+}
